ba86c885cf7fe6b0365d154c94bcd45ee894859e
Davi Lima <davi.n.lima99@gmail.com>
2024-08-26 00:05:18 -0300

    commit initial

---
+++ Application/UseCases/AddCustomer/AddCustomerUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts.UseCases.AddCustomer;
+
+namespace Application.UseCases.AddCustomer
+{
+    internal class AddCustomerUseCase : IAddCustomerUseCase
+    {
+    }
+}
---
+++ Domain/Contracts/UseCases/AddCustomer/IAddCustomerUseCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Contracts.UseCases.AddCustomer
+{
+    public interface IAddCustomerUseCase
+    {
+        void AddCustomer(Customer customer);
+    }
+}
